ef01e1061367fd43975f5ddc4b74673448e53939
Kien Ta <talekien1710@gmail.com>
2021-11-25 00:15:23 -0600

    Random Forest and Polynomial SVM

--- Assignment2_code/train.py
+++ Assignment2_code/train.py
@@ -44,7 +44,7 @@ plt.clf()
 
 # Save best model
 best_k = np.argmax(accuracies) + 1
-print('best k value for KNN', best_k)
+print('best k value for KNN', best_k, 'with mean accuracy', max(accuracies))
 best_models.append({
     'name': 'KNN',
     'model': KNeighborsClassifier(n_neighbors=best_k),
@@ -77,8 +77,74 @@ plt.clf()
 
 # Save best model
 best_max_depth = np.argmax(accuracies) + 1
-print('best max_depth value for Decision Tree', best_max_depth)
+print('best max_depth value for Decision Tree', best_max_depth, 'with mean accuracy', max(accuracies))
 best_models.append({
     'name': 'Decision Tree',
     'model': DecisionTreeClassifier(random_state=0, max_depth=best_max_depth),
+})
+
+print('--------------------------------------------------------------------------------------')
+print('Model: Random Forest')
+from sklearn.ensemble import RandomForestClassifier
+
+MAX_MAX_DEPTH = 30
+accuracies = np.zeros(MAX_MAX_DEPTH)
+for max_depth in range(1, MAX_MAX_DEPTH + 1):
+    model = RandomForestClassifier(random_state=0, max_depth=max_depth)
+    kf = KFold(n_splits=NUM_FOLDS)
+    mean_accuracy = 0
+    for i, (train_index, test_index) in enumerate(kf.split(X_train)):
+        X_train_fold, X_test_fold = X_train[train_index], X_train[test_index]
+        y_train_fold, y_test_fold = y_train_1d[train_index], y_train_1d[test_index]
+        model.fit(X_train_fold, y_train_fold)
+        mean_accuracy += accuracy(model, X_test_fold, y_test_fold)
+    mean_accuracy /= NUM_FOLDS
+    accuracies[max_depth - 1] = mean_accuracy
+# Plot accuracy
+plt.plot(range(1, MAX_MAX_DEPTH + 1), accuracies)
+plt.xlabel('max_depth')
+plt.ylabel('Accuracy')
+plt.title('Cross validation result of Random Forest')
+plt.savefig('./plots/Random-Forest.png')
+plt.clf()
+
+# Save best model
+best_max_depth = np.argmax(accuracies) + 1
+print('best max_depth value for Random Forest', best_max_depth, 'with mean accuracy', max(accuracies))
+best_models.append({
+    'name': 'Random Forest',
+    'model': RandomForestClassifier(random_state=0, max_depth=best_max_depth),
+})
+
+print('--------------------------------------------------------------------------------------')
+print('Model: SVM using the polynomial kernel')
+
+from sklearn.svm import SVC
+MAX_DEG = 30
+accuracies = np.zeros(MAX_DEG)
+for degree in range(1, MAX_DEG + 1):
+    model = SVC(kernel='poly', degree=degree)
+    kf = KFold(n_splits=NUM_FOLDS)
+    mean_accuracy = 0
+    for i, (train_index, test_index) in enumerate(kf.split(X_train)):
+        X_train_fold, X_test_fold = X_train[train_index], X_train[test_index]
+        y_train_fold, y_test_fold = y_train_1d[train_index], y_train_1d[test_index]
+        model.fit(X_train_fold, y_train_fold)
+        mean_accuracy += accuracy(model, X_test_fold, y_test_fold)
+    mean_accuracy /= NUM_FOLDS
+    accuracies[degree - 1] = mean_accuracy
+# Plot accuracy
+plt.plot(range(1, MAX_DEG + 1), accuracies)
+plt.xlabel('Degree')
+plt.ylabel('Accuracy')
+plt.title('Cross validation result of SVM using the polynomial kernel')
+plt.savefig('./plots/Polynomial-SVM.png')
+plt.clf()
+
+# Save best model
+best_degree = np.argmax(accuracies) + 1
+print('best max_depth value for Random Forest', best_degree, 'with mean accuracy', max(accuracies))
+best_models.append({
+    'name': 'Polynomial SVM',
+    'model': SVC(kernel='poly', degree=best_degree),
 })
